dbdfc52e4de2f173bde94f15f492f2d8d9399db1
A01653004 <A01653004@itesm.mx>
2022-09-02 17:27:08 -0500

    fix 2
    cam fix

---
+++ TC3003C71_Proyecto/Assets/AudioScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    AudioSource audio_;
+    [SerializeField] private AudioClip[] sonidos;
+    private void Awake()
+    {
+        audio_ = GetComponent<AudioSource>();
+
+    }
+
+    public void AudioSelect(int clip, float volume)
+    {
+        audio_.Stop();
+        audio_.PlayOneShot(sonidos[clip], volume);
+    }
+}

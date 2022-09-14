1ce05006d810c145f176e9174045dbba9f81451f
Diógenes Grajales Corona <a01653251@itesm.mx>
2022-09-14 01:55:26 -0500

    dmg updated and scenes changed

--- TC3003C71_Proyecto/Assets/Scripts/Damage.cs
+++ TC3003C71_Proyecto/Assets/Scripts/Damage.cs
@@ -12,10 +12,15 @@ public class Damage : MonoBehaviour
     private StarterAssets.ThirdPersonController tpc;
     private StarterAssets.StarterAssetsInputs sai;
 
+    Animator animator;
+    public Vector3 position_ = new Vector3(44.83648f, 0.4447613f, -3.45f);
+
 
 
     private void Awake()
     {
+        animator = gameObject.GetComponent<Animator>();
+
         if (transform.CompareTag("Player"))
         {
             childDetach = transform.GetChild(3);
@@ -28,11 +33,15 @@ public class Damage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (life <= 10)
+
+        if (life <= 0)
         {
             Death();
             childDetach.DetachChildren();
         }
+        else if (life > 0 && gameObject.CompareTag("Door")) {
+            animator.SetBool("Open", false);
+        } 
     }
 
     public void getDamage(int damage) {
@@ -66,11 +75,27 @@ public class Damage : MonoBehaviour
         
         UnityEngine.Color c = new UnityEngine.Color(1f, 0, 0);
 
+        if (gameObject.CompareTag("Player"))
+        {
 
+            //animacion de muerte
+
+            StartCoroutine(Respawn());
+        }
+        else if (gameObject.CompareTag("Door") && life <= 0)
+        {
+
+            if (life <= 0)
+            {
+                animator.SetBool("Open", true);
+            }
+
+
+        }
         // EDITAR ESTO PARA QUE SE MUERA EL PERSONAJE
         // Renderer rend = gameObject.GetComponent<Renderer>();
         // rend.material.SetColor("_Color", c);
-        
+
 
     }
     
@@ -95,4 +120,14 @@ public class Damage : MonoBehaviour
         }
         
     }
+
+    IEnumerator Respawn()
+    {
+        life = 100;
+        animator.SetTrigger("Dead");
+        yield return new WaitForSeconds(3.0f);
+
+        transform.position = position_;
+    }
+
 }
